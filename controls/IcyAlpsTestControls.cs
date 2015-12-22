154068b11e51aa73adab9f79b1db8d14a61c1eca
shaunvxc <shaun@yipitdata.com>
2015-12-22 10:23:40 -0500

    Reformat/Save working copy of test controls file
    this is a backup of the working touch screen controls before
    implementing the general framework for multiple types of controls
    scripts.

--- controls/IcyAlpsTestControls.cs
+++ controls/IcyAlpsTestControls.cs
@@ -4,90 +4,97 @@ using System.Collections;
 public class IcyAlpsTestControls : MonoBehaviour
 {
 
-    public delegate void SpeedUpEvent();
-    public static event SpeedUpEvent SpeedUp;
+  /*
+     Events/delegates to manage the speed of the level based on the players behaviors (aka the players controls)
+  */
 
-    public delegate void SlowDownEvent();
-    public static event SlowDownEvent SlowDown;
 
-    public float movementRate;
+  // these fire when the player straightens out downhill (ie not using the controls )
+  public delegate void SpeedUpEvent();
+  public static event SpeedUpEvent SpeedUp;
 
-    private Transform _transform;
-    private SpriteRenderer renderer;
-    private Sprite[] sprites;
+  // these fire when the player is turning (ie touching the screen, turning in some direction
+  public delegate void SlowDownEvent();
+  public static event SlowDownEvent SlowDown;
 
-    private float rotationSpeed = 1.2F;
-    public Quaternion localRotation;
-    public Vector3 eulerAngles;
+  public float movementRate;
 
-    void Awake() {
-	sprites = Resources.LoadAll<Sprite> ("SalmonAtlas");
-	_transform = transform;
-    }
+  private Transform _transform;
+  private SpriteRenderer renderer;
+  private Sprite[] sprites;
 
-    void Start ()
-    {
-	renderer = GetComponent<SpriteRenderer> ();
-	renderer.sprite = sprites [1];
-    }
+  private float rotationSpeed = 1.2F;
+  public Quaternion localRotation;
+  public Vector3 eulerAngles;
 
-    void Update ()
-    {
-	if (Input.touchCount > 0) {
-	    for (int i = 0; i < Input.touchCount; i++) {
+  void Awake() {
+    sprites = Resources.LoadAll<Sprite> ("SalmonAtlas");
+    _transform = transform;
+  }
 
-		Touch touch = Input.GetTouch (i);
-		Vector2 touchPosition = touch.position;
+  void Start ()
+  {
+    renderer = GetComponent<SpriteRenderer> ();
+    renderer.sprite = sprites [1];
+  }
 
-		touchPosition = Camera.main.ScreenToWorldPoint (touchPosition);
+  void Update ()
+  {
+    if (Input.touchCount > 0) {
+      for (int i = 0; i < Input.touchCount; i++) {
 
-		// Need to improve the physics for turning...
-		if(touchPosition.x < 0 ) {
-		    moveLeft();
-		}
-		else {
-		    moveRight();
-		}
-		
-		// localRotation = _transform.localRotation;
-		// eulerAngles = _transform.localRotation.eulerAngles;
-	    }
+	Touch touch = Input.GetTouch (i);
+	Vector2 touchPosition = touch.position;
+
+	touchPosition = Camera.main.ScreenToWorldPoint (touchPosition);
+
+	// Need to improve the physics for turning...
+	if(touchPosition.x < 0 ) {
+	  moveLeft();
 	}
 	else {
-	    // definitely don't NEED this check.. but should double check to make sure that calling Slerp when the object is already in postion
-	    // does not have any serious overhead
-	    if (_transform.localRotation.eulerAngles != new Vector3(0, 0, 0))  {
-		_transform.rotation = Quaternion.Slerp(_transform.localRotation, new Quaternion(0, 0, 0, 1F), Time.deltaTime * rotationSpeed);
-	    }
-	    
-	    // speed up once we straighten out!
-	    if (SpeedUp != null) {
-	    	SpeedUp();
-	    } 
+	  moveRight();
 	}
-    }
 
-    private void moveLeft() {
-	if ( _transform.localRotation.eulerAngles.z > 0F && _transform.localRotation.eulerAngles.z < 180F) {
-	    _transform.position = new Vector2(_transform.position.x + movementRate, transform.position.y);
-	    _transform.Rotate(_transform.localRotation * new Vector3(0, 0, -.5F));
-	    SpeedUp();
-	}
-	else {
-	    _transform.position = new Vector2(_transform.position.x - movementRate, transform.position.y);
-	    _transform.Rotate(_transform.localRotation * new Vector3(0, 0, -.5F));
-	    SlowDown();
-	}
+	// localRotation = _transform.localRotation;
+	// eulerAngles = _transform.localRotation.eulerAngles;
+      }
     }
+    else {
+      // definitely don't NEED this check.. but should double check to make sure that calling Slerp when the object is already in postion
+      // does not have any serious overhead
+      if (_transform.localRotation.eulerAngles != new Vector3(0, 0, 0))  {
+	_transform.rotation = Quaternion.Slerp(_transform.localRotation, new Quaternion(0, 0, 0, 1F), Time.deltaTime * rotationSpeed);
+      }
 
-    private void moveRight() {
-	if ( _transform.localRotation.eulerAngles.z > 180F && _transform.localRotation.eulerAngles.z < 360F) {
-	    _transform.position = new Vector2(_transform.position.x - movementRate, transform.position.y);
-	    _transform.Rotate(_transform.localRotation * new Vector3(0, 0, .5F));
-	    SpeedUp();
-	}
-	else {
-	    _transform.position = new Vector2(_transform.position.x + movementRate, transform.position.y);
+      // speed up once we straighten out!
+      if (SpeedUp != null) {
+	SpeedUp();
+      }
+    }
+  }
+
+  private void moveLeft() {
+    if ( _transform.localRotation.eulerAngles.z > 0F && _transform.localRotation.eulerAngles.z < 180F) {
+      _transform.position = new Vector2(_transform.position.x + movementRate, transform.position.y);
+      _transform.Rotate(_transform.localRotation * new Vector3(0, 0, -.5F));
+      SpeedUp();
+    }
+    else {
+      _transform.position = new Vector2(_transform.position.x - movementRate, transform.position.y);
+      _transform.Rotate(_transform.localRotation * new Vector3(0, 0, -.5F));
+      SlowDown();
+    }
+  }
+
+  private void moveRight() {
+    if ( _transform.localRotation.eulerAngles.z > 180F && _transform.localRotation.eulerAngles.z < 360F) {
+      _transform.position = new Vector2(_transform.position.x - movementRate, transform.position.y);
+      _transform.Rotate(_transform.localRotation * new Vector3(0, 0, .5F));
+      SpeedUp();
+    }
+    else {
+      _transform.position = new Vector2(_transform.position.x + movementRate, transform.position.y);
 	    _transform.Rotate(_transform.localRotation * new Vector3(0, 0, .5F));
 	    SlowDown();
 	}
