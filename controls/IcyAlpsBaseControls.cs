87e3de96cbe7d07690046f3424594b091cd87f53
shaunvxc <shaun@yipitdata.com>
2015-12-31 16:18:32 -0500

    expose editor vars for tuning turn rate and the tile atlas, formatting

--- controls/IcyAlpsBaseControls.cs
+++ controls/IcyAlpsBaseControls.cs
@@ -8,8 +8,7 @@ using System.Collections;
 public class IcyAlpsBaseControls : MonoBehaviour
 {
 
-  public virtual bool isMoving ()
-  {
+  public virtual bool isMoving () {
     Debug.Log ("Error! Using controls script with no implementation of isMoving()-- this means the controls will not be responsive!");
     return false;
   }
@@ -19,8 +18,7 @@ public class IcyAlpsBaseControls : MonoBehaviour
     return false;
   }
 
-  public virtual bool isMovingLeft ()
-  {
+  public virtual bool isMovingLeft () {
     Debug.Log ("Error! Using controls script with no implementation of isMovingLeft()-- this means the controls will not be responsive!");
     return false;
   }
@@ -34,33 +32,33 @@ public class IcyAlpsBaseControls : MonoBehaviour
   public static event SlowDownEvent SlowDown;
 
   public float movementRate;
+  public Quaternion localRotation;
+  public Vector3 eulerAngles;
+
+  public string atlas;
+  public float turnRate;
 
   private Transform _transform;
   private SpriteRenderer renderer;
   private Sprite[] sprites;
-
   private float rotationSpeed = 1.2F;
-  public Quaternion localRotation;
-  public Vector3 eulerAngles;
 
   void Awake() {
-    sprites = Resources.LoadAll<Sprite> ("SalmonAtlas");
+    sprites = Resources.LoadAll<Sprite> (atlas);
     _transform = transform;
   }
 
-  void Start ()
-  {
+  void Start () {
     renderer = GetComponent<SpriteRenderer> ();
-    renderer.sprite = sprites [1];
+    renderer.sprite = sprites [0];
   }
 
-  void Update ()
-  {
+  void Update () {
     if (isMoving()) {
       if (isMovingLeft()) {
  	moveLeft();
       }
-      else if(isMovingRight()) {
+      else if (isMovingRight()) {
 	moveRight();
       }
     }
@@ -70,42 +68,42 @@ public class IcyAlpsBaseControls : MonoBehaviour
   }
 
   private void moveLeft() {
-    if ( _transform.localRotation.eulerAngles.z > 0F && _transform.localRotation.eulerAngles.z < 180F) {
+    if (_transform.localRotation.eulerAngles.z > 0F && _transform.localRotation.eulerAngles.z < 180F) {
       _transform.position = new Vector2(_transform.position.x + movementRate, _transform.position.y);
-      _transform.Rotate(_transform.localRotation * new Vector3(0, 0, -.5F));
+      _transform.Rotate(_transform.localRotation * new Vector3(0, 0, -turnRate));
       SpeedUp();
     }
     else {
       _transform.position = new Vector2(_transform.position.x - movementRate, _transform.position.y);
-      _transform.Rotate(_transform.localRotation * new Vector3(0, 0, -.5F));
+      _transform.Rotate(_transform.localRotation * new Vector3(0, 0, -turnRate));
       SlowDown();
     }
   }
 
   private void moveRight() {
-    if ( _transform.localRotation.eulerAngles.z > 180F && _transform.localRotation.eulerAngles.z < 360F) {
+    if (_transform.localRotation.eulerAngles.z > 180F && _transform.localRotation.eulerAngles.z < 360F) {
       _transform.position = new Vector2(_transform.position.x - movementRate, _transform.position.y);
-      _transform.Rotate(_transform.localRotation * new Vector3(0, 0, .5F));
+      _transform.Rotate(_transform.localRotation * new Vector3(0, 0, turnRate));
       SpeedUp();
     }
     else {
       _transform.position = new Vector2(_transform.position.x + movementRate, _transform.position.y);
-      _transform.Rotate(_transform.localRotation * new Vector3(0, 0, .5F));
+      _transform.Rotate(_transform.localRotation * new Vector3(0, 0, turnRate));
       SlowDown();
     }
   }
 
   private void straightenOut() {
-      // definitely don't NEED this check.. but should double check to make sure that calling Slerp when the object is already in postion
-      // does not have any serious overhead
-      if (_transform.localRotation.eulerAngles != new Vector3(0, 0, 0))  {
-	_transform.rotation = Quaternion.Slerp(_transform.localRotation, new Quaternion(0, 0, 0, 1F), Time.deltaTime * rotationSpeed);
-      }
+    // definitely don't NEED this check.. but should double check to make sure that calling Slerp when the object is already in postion
+    // does not have any serious overhead
+    if (_transform.localRotation.eulerAngles != new Vector3(0, 0, 0)) {
+      _transform.rotation = Quaternion.Slerp(_transform.localRotation, new Quaternion(0, 0, 0, 1F), Time.deltaTime * rotationSpeed);
+    }
 
-      // speed up once we straighten out!
-      if (SpeedUp != null) {
-	SpeedUp();
-      }
+    // speed up once we straighten out!
+    if (SpeedUp != null) {
+      SpeedUp();
+    }
   }
 
 }
