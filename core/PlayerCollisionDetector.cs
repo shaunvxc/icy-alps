82466f3f6eb952710d8687e57df7a417613fe8bf
shaunvxc <shaun@yipitdata.com>
2016-01-03 18:42:05 -0500

    remove unneeded collider reference

--- core/PlayerCollisionDetector.cs
+++ core/PlayerCollisionDetector.cs
@@ -11,12 +11,6 @@ public class PlayerCollisionDetector : MonoBehaviour {
   public delegate void PlayerDeathEvent();
   public static event PlayerDeathEvent PlayerDeath;
 
-  private BoxCollider2D  collider;
-
-  void Awake() {
-    collider = GetComponent<BoxCollider2D>();
-  }
-
   void OnTriggerEnter2D (Collider2D collision) {
     if (PlayerDeath != null) {
       Debug.Log("A Player death should occur here..");
